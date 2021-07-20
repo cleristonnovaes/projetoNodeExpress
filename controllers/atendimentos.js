ea386ab30a124c17c7d18006ba5801ddc821b314
Cleriston <novaes.cleriston@gmail.com>
2021-07-20 14:40:36 -0300

    Finalizado

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,30 @@
-module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+const Atendimento = require('../models/atendimentos')
 
-    app.post('/atendimentos', (req, res) =>{ 
-        console.log(req.body)
-        res.send('Você está na rota de atendimento e está realizando um POST')
+module.exports = app => {
+    app.get('/atendimentos', (req,res) =>{
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) =>{
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorId(id, res)
+    })
+
+    app.post('/atendimentos', (req, res) =>{
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch('/atendimentos/:id', (req, res) =>{
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) =>{
+        const id = parseInt(req.params.id)
+
+        Atendimento.deleta(id, res)
     })
 }
